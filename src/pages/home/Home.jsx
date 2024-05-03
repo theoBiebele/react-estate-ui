774afd8d0e3daec1de9dfe093f91acc4331a2a56
theoBiebele <theobiebele@gmail.com>
2024-05-03 11:39:20 +0100

    completed Homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
 import './home.scss';
+import SearchBar from '../../components/Navbar/searchBar/searchBar';
 
 
 
 const Home = () => {
   return (
     <div className='homePage'>
-      <div className="textcontainer"></div>
-      <div className="textcontainer"></div>
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className='title'>
+            Find Real Estate & Get YOur Dream Place
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro et dolorum nostrum voluptate, nisi reiciendis error consequuntur in consectetur! Impedit a, deleniti itaque qui quis voluptas quas provident obcaecati at.
+          </p>
+
+          <SearchBar />
+
+          <div className="boxes">
+            <div className="box">
+              <h2>16+</h2>
+              <h3>Years of Experience</h3>
+            </div>
+
+            <div className="box">
+              <h2>200</h2>
+              <h3>Award Gained</h3>
+            </div>
+
+            <div className="box">
+              <h2>1200+</h2>
+              <h3>Properties Ready</h3>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
     </div>
   )
 }
